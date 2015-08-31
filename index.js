604393794827c524dc0e7c61c32512a108b2469e
Kevin King <king.p.kevin@gmail.com>
2015-08-31 15:19:04 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+var is_handled = false
+var spawn = require('child_process').spawn;
+var child
+setInterval(function() {
+  is_handled = false
+}, 60000)
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
@@ -17,6 +23,21 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+  if(!is_handled) {
+    child = spawn('salt', ['"qa"', 'state.apply', 'qa.git']);
+    
+    child.stdout.on('data', function(chunk) {
+      console.log('stdout: ' + chunk);
+    });
+    child.stderr.on('data', function(data) {
+      console.log('stderr: ' + data);
+    });
+    child.on('close', function(code) {
+      console.log('closing code: ' + code);
+    });
+  }
+  is_handled = true
+
 })
 
 handler.on('issues', function (event) {
